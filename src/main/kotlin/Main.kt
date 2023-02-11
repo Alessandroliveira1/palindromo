3f0b623b3cedd14d75b0ed5e689daac189d98137
DaianeArrais <42252229+DaianeArrais@users.noreply.github.com>
2023-02-11 12:19:23 -0300

    fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 import java.util.*
 
+private val regex = "[ -/:-@\\[-`{-~áéíóúâêîôûãõç’]".toRegex()
+private const val EMPTY = ""
+
 fun main(args : Array<String>) {
     val reader = Scanner(System.`in`)
     print("Escreva uma palavra: ")
@@ -14,14 +17,13 @@ fun main(args : Array<String>) {
 
 
 fun isPalindromo(value: String): Boolean {
-    val regex = "[ -/:-@\\[-`{-~áéíóúâêîôûãõç’]".toRegex()
-    var filterValue = ""
-    value.forEach {
-        if(!regex.matches(it.toString())) {
-            filterValue = it + filterValue
-        } else{
-            value.removePrefix(it.toString())
-        }
+    var filterValue = EMPTY
+    for (i in value.length - 1 downTo 0) {
+       if (!regex.matches(value[i].toString())) {
+           filterValue += value[i]
+       }else {
+           value.removePrefix(value[i].toString())
+       }
     }
-    return  regex.replace(value, "").lowercase() == filterValue.lowercase()
+    return  regex.replace(value, EMPTY).lowercase() == filterValue.lowercase()
 }
